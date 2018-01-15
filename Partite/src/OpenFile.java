a20e6929c17f3cc99eb70474ab011f8a0b2c3a41
HexRebuilt <32871843+HexRebuilt@users.noreply.github.com>
2018-01-15 12:15:14 +0100

    HexRebuilt-t300
    da sistemare la lettura del file

---
+++ Partite/src/OpenFile.java
@@ -0,0 +1,66 @@
+import match.Calcio;
+import match.Pallavvolo;
+import match.Partita;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class OpenFile {
+    public String digitata;
+    private BufferedReader lettore;
+    private FileReader file;
+    private ArrayList<String[]> riga;
+
+    public OpenFile() {
+        this.digitata=null;
+        this.lettore=null;
+        this.file=null;
+        this.riga=new ArrayList<>();
+    }
+
+    public String tastiera() {
+        InputStreamReader keyboard = new InputStreamReader(System.in);
+        BufferedReader buff = new BufferedReader(keyboard);
+        try {
+            digitata= buff.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keyboard.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return digitata;
+    }
+
+    public Partita apriFile(String nome_file){
+        try {
+            file = new FileReader(nome_file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        lettore = new BufferedReader(file);
+
+        if(nome_file.equals("calcio.txt")){
+            return new Calcio();
+        }
+        else{
+            return new Pallavvolo();
+        }
+    }
+
+    public ArrayList leggi(){
+        String line=null;
+        while((line=lettore.readLine())!=null){
+            String[] elemento=line.split("\t");
+            riga.add(elemento);
+        }
+
+
+
+        return riga;
+    }
+
+}
--- partite/src/test.java
+++ partite/src/test.java
@@ -6,33 +6,17 @@ import java.io.*;
 
 public class test {
     public static void main(String[] args) throws IOException {
-
-        InputStreamReader keyboard=new InputStreamReader(System.in);
-        BufferedReader buff=new BufferedReader(keyboard);
-        String nome_file=buff.readLine();
-        try {
-            keyboard.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        OpenFile openfile=new OpenFile();
+        String nome_file=openfile.tastiera();
 
         nome_file=nome_file.toLowerCase();
-        Partita game;
-
-        //TODO sistemare apertura file
-
-        FileReader file=new FileReader(nome_file);
-        BufferedReader lettore = new BufferedReader(file);
-
-        if (nome_file.equals("calcio.txt")){
-            game=new Calcio();
-        }
-        else {
-            game=new Pallavvolo();
-        }
+        Partita game=openfile.apriFile(nome_file);
 
 
 
-        lettore.close();
+
+
+
+
     }
 }
