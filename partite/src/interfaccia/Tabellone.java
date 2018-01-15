e63c8cbe25a1b8c61fdbe0204b3f476d1411adb5
HexRebuilt <HexRebuilt@users.noreply.github.com>
2018-01-15 22:13:56 +0100

    HexRebuilt
    Creazione tabellone segnapunti

--- partite/src/interfaccia/Tabellone.java
+++ partite/src/interfaccia/Tabellone.java
@@ -4,18 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Tabellone extends JPanel {
-    public Tabellone(String casa, String ospiti, String dateEvent){
+    public Tabellone(String dateEvent, int ptC, int ptO, int setC, int setO, int set, String minuti){
         setLayout(new GridLayout(3,3));
+
         //todo layout team A, tempo gioco, Team B \n pt A, data match, pt B, time out A, primo/secondo tempo, time out B
-        /*add(),add(),add();
-        add(),add(),add();
-        add(),add(),add()
-        */
-    }
+        JLabel casa=new JLabel("Casa");
+        JLabel time=new JLabel(minuti);
+        JLabel ospiti=new JLabel("Ospiti");
+        add(casa);add(time);add(ospiti);
 
-    public class squadra extends JLabel{
-        public squadra (){
+        JLabel pointsC= new JLabel("Punti:\n"+ptC);
+        JLabel tempo=new JLabel("Tempo/Set:\n"+set);
+        JLabel pointsO=new JLabel("Punti:\n"+ptO);
+        add(pointsC);add(tempo);add(pointsO);
 
-        }
+        JLabel setVintiC= new JLabel("Set Vinti:\n"+setC);
+        JButton update=new JButton();
+        JLabel setVintiO=new JLabel("Set Vinti:\n"+setO);
+        add(setVintiC);add(update);add(setVintiO);
     }
 }
