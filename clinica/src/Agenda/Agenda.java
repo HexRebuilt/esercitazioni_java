457fde3ac79e69cdf010f2c2e1f0f01354a97375
HexRebuilt <32871843+HexRebuilt@users.noreply.github.com>
2018-03-13 14:37:43 +0100

    last

--- clinica/src/Agenda/Agenda.java
+++ clinica/src/Agenda/Agenda.java
@@ -8,7 +8,7 @@ public class Agenda {
     private final int[] giorniLavorativi= new int[]{1,2,3,4,5},oreLavorative=new int[]{8,9,10,11,12,13,14,15,16,17};
     private ArrayList<Appuntamenti> appuntamenti;
 
-    
+
 
     private void creaAppuntamenti() {
         String[] letta;
